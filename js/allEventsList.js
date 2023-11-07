ac6d29176e05fed4d2f6d605cf8e9bedc8487b8e
shihabfaruk <sfaruk3131p@gmail.com>
2023-11-07 18:22:49 -0500

    Edited the array descriptions of events

--- js/allEventsList.js
+++ js/allEventsList.js
@@ -36,7 +36,7 @@ allEventsList.push(new Event(
 	"Topfer Rehearsal Studio", // location
 	"11/14/23", // date
 	"event-3.png", // imageFile
-	"[Description forthcoming]", // description
+	"Brooklyn College students come to the Electro-acoustic Music Festival to experience cutting edge works by masters and emerging composers. This event is free to all students and no prior knowledge is needed to enjoy the event. Reminder please do not eat in the venue and remember to be careful with your drinks. We hope to see you there!", // description
 	["music"], // eventTypes
 	[6, 1] // relatedEvents
 ));
@@ -47,7 +47,7 @@ allEventsList.push(new Event(
 	"Topfer Rehearsal Studio", // location
 	"12/10/23", // date
 	"event-4.png", // imageFile
-	"[Description forthcoming]", // description
+	"Brooklyn College students come to the Afternoons at the Piano 2 to see the Conservatory of Music pianists perform solo works. The event will be about an hour long in the Topfer Rehearsal studio with 5 minute breaks between each performance. The event is free of charge and anyone may attend. No food or drinks are allowed in the venue and please be mindful of your manners during the performances. We hope to see you there!", // description
 	["music"], // eventTypes
 	[6, 1] // relatedEvents
 ));
@@ -58,7 +58,7 @@ allEventsList.push(new Event(
 	"Don Buchwald Theater", // location
 	"12/21/23", // date
 	"event-5.png", // imageFile
-	"[Description forthcoming]", // description
+	"Brooklyn College students come see the play El Huracán performed in the Don Buchwald Theater. A powerful tale of family and forgiveness, El Huracán, written by Miami native and recent Academy Award winner Charise Castro Smith (Disney's Encanto) reveals what can be rebuilt in the aftermath of life's most devastating tempests and what can never be replaced. This event requires tickets to gain admission. No food or drinks in the venue. We hope to see you soon!", // description
 	["theater"], // eventTypes
 	[6, 2] // relatedEvents
 ));
@@ -69,7 +69,7 @@ allEventsList.push(new Event(
 	"New Workshop Theater", // location
 	"1/17/24", // date
 	"event-6.png", // imageFile
-	"[Description forthcoming]", // description
+	"Brooklyn College students come see the play Eurydice performed in the New Workshop Theater. Eurydice is a 2003 play by Sarah Ruhl which retells the myth of Orpheus from the perspective of Eurydice, his wife. The story focuses on Eurydice's choice to return to Earth with Orpheus or to stay in the underworld with her father. Admission requires tickets to be shown at the entrance. No food or drinks in the venue. We hope to see you there!", // description
 	["theater"], // eventTypes
 	[5, 1] // relatedEvents
 ));
@@ -80,7 +80,7 @@ allEventsList.push(new Event(
 	"Student Center", // location
 	"2/12/24", // date
 	"event-7.png", // imageFile
-	"[Description forthcoming]", // description
+	"Brooklyn College students come on down to the BC Job Fair to meet representatives of companies such as Google, Meta, Chase on job opportunities. Students will have a chance to discuss with representatives of these companies on internship opportunities as well as what employers look at when assessing potential employees. This event is free of charge. We hope to see you there!", // description
 	["recruiting"], // eventTypes
 	[8, 2] // relatedEvents
 ));
@@ -91,7 +91,7 @@ allEventsList.push(new Event(
 	"148 Ingersoll Hall", // location
 	"2/24/24", // date
 	"event-8.png", // imageFile
-	"[Description forthcoming]", // description
+	"Brooklyn College students come down to 148 Ingersoll Hall to attend a presentation held by experienced software developers in the industry to hear about there experiences and possible advice to building up your resume. These lectures will involve developers from companies such as Google, Meta, Twitter, and many more. We will also host a small workshop on how to build a portfolio and your resume to prepare you for the job hunt. This event is free of charge. We hope to see you there!", // description
 	["recruiting"], // eventTypes
 	[7, 5] // relatedEvents
 ));
