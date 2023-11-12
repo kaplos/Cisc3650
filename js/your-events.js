f742dca23fcff1fcf7b4665bb4ebb1c0621b8bd5
Lucius Kwok <lk@felttip.com>
2023-11-12 07:44:03 -0500

    Added placeholders to Your Events
    For each section in Your Events, if there are no events, a placeholder div is shown instead of an empty list.

--- js/your-events.js
+++ js/your-events.js
@@ -5,20 +5,25 @@
 "use strict";
 
 function addEvents(section, list) {
-	let container = document.getElementById(section);
+	let container = document.getElementById(section+"Section");
 
+	// Add events
 	list.forEach((identifier, index) => {
 		const event = allEventsList.find(x => (x.identifier == identifier));
 		if (event) container.appendChild(event.createDiv(true));
 	});
+	
+	// Hide placeholder
+	let placeholder = document.getElementById(section+"PlaceholderStatic");
+	placeholder.hidden = (list.length > 0);
 }
 
 function removeEvents(section) {
 	// Remove all elements except the header from the section
-	let container = document.getElementById(section);
+	let container = document.getElementById(section+"Section");
 	let toRemove = [];
 	for (const child of container.children) {
-		if (!child.id.endsWith("Header")) toRemove.push(child);
+		if (!child.id.endsWith("Static")) toRemove.push(child);
 	}
 	for (const child of toRemove) container.removeChild(child);
 }
@@ -26,6 +31,6 @@ function removeEvents(section) {
 // Main
 // Populate the list with events the user is going to or is interested in
 loadEventsFromLocalStorage();
-addEvents("goingSection", goingEvents);
-addEvents("interestedSection", interestedEvents);
+addEvents("going", goingEvents);
+addEvents("interested", interestedEvents);
 
