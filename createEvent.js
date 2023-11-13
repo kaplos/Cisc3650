2f1e807501c455181e5081b954b5c297b83e2d17
chaim mallakh <34977295+kaplos@users.noreply.github.com>
2023-11-13 14:56:54 -0800

    minor revisons to form validation

--- createEvent.js
+++ createEvent.js
@@ -4,16 +4,22 @@ let inputArray = Array.from(allInput);
 inputArray.push(textarea);
 let submitButton = document.querySelector("#submitForm");
 let dateInput = document.querySelector("#dateInput");
-let today = new Date().toISOString().split('T')[0];
-dateInput.min = today;
+let today = new Date();
+dateInput.min = today.toISOString().split('T')[0];
+
+let nextYear = new Date();
+nextYear.setFullYear(today.getFullYear() + 1);
+dateInput.max = nextYear.toISOString().split('T')[0];
 
 
 function validateForm() {
     let isValid = true;
     inputArray.forEach((input) => {
-        if(input.id='dateInput'){
-
-        }
+        // if(input.id='dateInput'){
+        //     let inputDate = new Date(input.value);
+        //     inputDate.setHours(0, 0, 0, 0);
+           
+        // }
         if (input.value.trim() === '' && input.type !== 'file') {
             isValid = false;
             input.classList.add('error');// Add error class to show some visual feedback
@@ -26,6 +32,7 @@ function validateForm() {
 }
 
 submitButton.addEventListener('click', (event) => {
+    console.log('submit button pressed');
     event.preventDefault(); // Prevent form from submitting
     if (validateForm()) {
         window.location.href = "./submitPage.html"
